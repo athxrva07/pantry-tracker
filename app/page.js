c63202954e0c553be00a2769f4b84d91792369e9
Atharva <atharvavc04@gmail.com>
2024-08-25 02:39:14 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -124,7 +124,7 @@ export default function Home() {
               Pantry Items
             </Typography>
         </Box>
-      </Box>
+      
       <Stack width="800px" height="300px" spacing={2} overflow="auto">
         {inventory.map(({name, quantity})=>(
            <Box 
@@ -137,17 +137,27 @@ export default function Home() {
              bgcolor="#f0f0f0"
              padding={5}
            >
-            <Typography variant="h3" color="#333" textAlign="center">
+            <Typography variant="h2" color="#333" textAlign="center">
               {name.charAt(0).toUpperCase() + name.slice(1)}
             </Typography>
 
-            <Typography variant="h3" color="#333" textAlign="center">
+            <Typography variant="h2" color="#333" textAlign="center">
               {quantity}
             </Typography>
+           <Stack direction="row" spacing={2}>
+            <Button variant="contained" onClick={()=>{
+              addItem(name)
+            }}>Add</Button>
+
+            <Button variant="contained" onClick={()=>{
+              removeItem(name)
+            }}>Remove</Button>
+           </Stack>
 
            </Box>
           ))}
       </Stack>
+      </Box>
     </Box>
   );
 }
